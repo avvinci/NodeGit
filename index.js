d9f33f5e4f22f6888feb271a5c81a7344ae27485
avvinci <vinciabhinav7@gmail.com>
2019-07-27 12:13:14 +0530

    added inquirer for terminal input

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 const chalk = require("chalk");
 const clear = require("clear");
 const figlet = require("figlet");
+const files = require("./lib/files");
+const inquirer = require("./lib/inquirer");
+
+clear();
+console.log(
+  chalk.yellow(
+    figlet.textSync("A journey begins", { horizontalLayout: "full" })
+  )
+);
+
+// if (files.directoryExists(".git")) {
+//   console.log(chalk.red(" A git repo"));
+//   process.exit();
+// }
+
+const run = async () => {
+  const credentials = await inquirer.askGithubCredentials();
+  console.log(credentials);
+};
+
+run();
